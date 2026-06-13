4aa7c783213356df734c236389bd562c908db4da
Yash Pingat <ynpingat@gmail.com>
2026-06-13 18:06:14 +0530

    feat: create initial contract model

--- contracts/models.py
+++ contracts/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Contract(models.Model):
+    title = models.CharField(max_length=255)
+    uploaded_file = models.FileField(upload_to='contracts/')
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
