db21d532bd8df637dd55744b65d37427cdc3b2d2
Yash Pingat <ynpingat@gmail.com>
2026-06-17 00:02:41 +0530

    feat: implement automatic PDF text extraction and storage

--- contracts/models.py
+++ contracts/models.py
@@ -1,10 +1,25 @@
 from django.db import models
+from .utils import extract_text_from_pdf
 
 class Contract(models.Model):
     title = models.CharField(max_length=255)
     uploaded_file = models.FileField(upload_to='contracts/')
+    extracted_text = models.TextField(blank=True)
     uploaded_at = models.DateTimeField(auto_now_add=True)
 
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+        if self.uploaded_file and not self.extracted_text:
+            try:
+                self.extracted_text = extract_text_from_pdf(
+                    self.uploaded_file.path
+                )
+                super().save(update_fields=['extracted_text'])
+            except Exception as e:
+                print("Extraction Error:", e)
+    
+    
     def __str__(self):
         return self.title
 
