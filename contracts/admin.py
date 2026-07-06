a94aac93dace4179b52418e805f23e92c21cee8f
Yash Pingat <ynpingat@gmail.com>
2026-07-06 10:40:22 +0530

    feat: configure Django admin for contract management

--- contracts/admin.py
+++ contracts/admin.py
@@ -1,6 +1,71 @@
 from django.contrib import admin
 from .models import Contract, ExtractedClause, RiskFlag
 
-admin.site.register(Contract)
-admin.site.register(ExtractedClause)
-admin.site.register(RiskFlag)
+
+@admin.register(Contract)
+class ContractAdmin(admin.ModelAdmin):
+
+    list_display = (
+        "id",
+        "title",
+        "uploaded_at"
+    )
+
+    search_fields = (
+        "title",
+    )
+
+    list_filter = (
+        "uploaded_at",
+    )
+
+    ordering = (
+        "-uploaded_at",
+    )
+
+
+@admin.register(ExtractedClause)
+class ExtractedClauseAdmin(admin.ModelAdmin):
+
+    list_display = (
+        "id",
+        "contract",
+        "clause_type"
+    )
+
+    search_fields = (
+        "clause_type",
+        "clause_text",
+        "contract__title"
+    )
+
+    list_filter = (
+        "clause_type",
+    )
+
+    ordering = (
+        "contract",
+    )
+
+
+@admin.register(RiskFlag)
+class RiskFlagAdmin(admin.ModelAdmin):
+
+    list_display = (
+        "id",
+        "contract",
+        "risk_level"
+    )
+
+    search_fields = (
+        "description",
+        "contract__title"
+    )
+
+    list_filter = (
+        "risk_level",
+    )
+
+    ordering = (
+        "contract",
+    )
